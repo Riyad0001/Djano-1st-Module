9d92e61ac778ff9e2b55f5bd4376a6be9ca440e8
Riyad0001 <rieadhasan499@gmail.com>
2024-07-14 03:52:22 +0600

    fast c

---
+++ project_1/project_1/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+
+def contact(request):
+    return HttpResponse("This is Contact Page,Riyad")
+def nai(request):
+    return HttpResponse("Riyad nai R drvdrvdrvdrvdrvdrvdrvdrvdrvdrv")
