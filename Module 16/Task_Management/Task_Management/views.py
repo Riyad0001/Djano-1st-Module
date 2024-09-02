4ae80b8e19628d3ac9b988028f2628c5aaea87b0
Riyad0001 <rieadhasan499@gmail.com>
2024-09-02 18:12:03 +0600

    Nine

--- Module 16/Task_Management/Task_Management/views.py
+++ Module 16/Task_Management/Task_Management/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from task import models
 
 def home(request):
-    data=models.TaskModel.objects.all()
+    data=models.TaskModel.objects.prefetch_related('catagory_name').all()
     return render(request,"home.html",{'data':data})
--- Module 16/Task_Management/task/views.py
+++ Module 16/Task_Management/task/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from . import forms
+from . import models
 # Create your views here.
 def add_task(request):
     if request.method=="POST":
@@ -10,3 +11,17 @@ def add_task(request):
     else:
         form=forms.TaskForm()
     return render(request,"add_task.html",{'form':form})
+def edit_task(request,id):
+    post=models.TaskModel.objects.get(pk=id)
+    form=forms.TaskForm(instance=post)
+    if request.method=="POST":
+        form=forms.TaskForm(request.POST,instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
+    return render(request,"add_task.html",{'form':form})
+
+def delete_task(request,id):
+    post=models.TaskModel.objects.get(pk=id)
+    post.delete()
+    return redirect('homepage')
