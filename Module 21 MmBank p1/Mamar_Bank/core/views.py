f36064c37e196adc299ec562295eeeb6c1502886
Md Harun Or Roshed <110784387+Riyad0001@users.noreply.github.com>
2024-12-11 21:32:08 +0600

    Delete Module 21 MmBank p1 directory

--- Module 21 MmBank p1/Mamar_Bank/core/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render,redirect
-from django.views.generic import TemplateView
-# Create your views here.
-
-class HomeVaw(TemplateView):
-    template_name='index.html'
